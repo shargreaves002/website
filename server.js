a666f5a9c361841fecaea18b8892257971231905
Sarah <sarahhargreaves10@gmail.com>
2019-11-02 19:05:25 -0400

    Testing pruned code

--- server.js
+++ server.js
@@ -2,10 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const projectName = "website";
-const bodyParser = require('body-parser');
 const { google } = require("googleapis");
 const fs = require('fs');
-const readline = require('readline');
+// const readline = require('readline');
 
 app.use(express.json());
 app.use(express.static('dist/' + projectName));
@@ -37,60 +36,51 @@ app.listen(app.get('port'), function () {
 
 app.post('/api/contact', (req, res) => {
 
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
   const TOKEN_PATH = 'token.json';
 
 // Load client secrets from a local file.
   fs.readFile('credentials.json', (err, content) => {
-    if (err) return console.log('Error loading client secret file:', err);
+    if (err) throw err;
     // Authorize a client with credentials, then call the Gmail API.
-    authorize(JSON.parse(content), sendMessage);
+    authorize(JSON.parse(content.toString()), sendMessage);
   });
 
-  /**
-   * Create an OAuth2 client with the given credentials, and then execute the
-   * given callback function.
-   * @param {Object} credentials The authorization client credentials.
-   * @param {function} callback The callback to call with the authorized client.
-   */
   function authorize(credentials, callback) {
     const {client_secret, client_id, redirect_uris} = credentials.installed;
     const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
 
     fs.readFile(TOKEN_PATH, (err, token) => {
-      if (err) return getNewToken(oAuth2Client, callback);
-      oAuth2Client.setCredentials(JSON.parse(token));
+      if (err) throw err;
+      oAuth2Client.setCredentials(JSON.parse(token.toString()));
       callback(oAuth2Client);
     });
   }
 
-  function getNewToken(oAuth2Client, callback) {
-    const authUrl = oAuth2Client.generateAuthUrl({
-      access_type: 'offline',
-      scope: SCOPES,
-    });
-    console.log('Authorize this app by visiting this url:', authUrl);
-    const rl = readline.createInterface({
-      input: process.stdin,
-      output: process.stdout,
-    });
-    rl.question('Enter the code from that page here: ', (code) => {
-      rl.close();
-      oAuth2Client.getToken(code, (err, token) => {
-        if (err) return console.error('Error retrieving access token', err);
-        oAuth2Client.setCredentials(token);
-        // Store the token to disk for later program executions
-        fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
-          if (err) return console.error(err);
-          console.log('Token stored to', TOKEN_PATH);
-        });
-        callback(oAuth2Client);
-      });
-    });
-  }
 
+  /*function getNewToken(oAuth2Client, callback) {
+      const authUrl = oAuth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: SCOPES,
+      });
+      console.log('Authorize this app by visiting this url:', authUrl);
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+      });
+      rl.question('Enter the code from that page here: ', (code) => {
+        rl.close();
+        oAuth2Client.getToken(code, (err, token) => {
+          if (err) return console.error('Error retrieving access token', err);
+          oAuth2Client.setCredentials(token);
+          // Store the token to disk for later program executions
+          fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+            if (err) return console.error(err);
+            console.log('Token stored to', TOKEN_PATH);
+          });
+          callback(oAuth2Client);
+        });
+      });
+    }*/
   function makeBody(to, from, subject, message) {
     const str = ["Content-Type: text/plain; charset=\"UTF-8\"\n",
       "MIME-Version: 1.0\n",
@@ -105,11 +95,10 @@ app.post('/api/contact', (req, res) => {
   }
 
   function sendMessage(auth) {
-    console.log(req.body);
     const raw = makeBody('sarahhargreaves10@gmail.com',
                       'sarahhargreaves10@gmail.com',
                     req.body.name + ' would like to contact you!',
-                  '<' + req.body.email + '> says: ' + req.body.message);
+                  'Please reply to:' + req.body.email + '\n\n\n' + req.body.message);
     const gmail = google.gmail({version: 'v1', auth});
     gmail.users.messages.send({
       auth: auth,
